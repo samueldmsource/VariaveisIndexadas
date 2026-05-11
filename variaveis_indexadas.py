3a917837debd161903cd92a3d3ab32f431deeffd
samueldmsource <samuelsjnmg@gmail.com>
2026-05-11 11:04:44 -0300

    Renomeado arquivo

---
+++ variaveis_indexadas.py
@@ -0,0 +1,50 @@
+'''numeros=[-5,-2,5,7,8,9]
+negativo=[i for i in numeros if i < 0]
+print(negativo)'''
+
+'''Dados=[4,7,8,9,25]
+maximo=max(Dados)
+position=Dados.index(maximo)
+
+print("valor maximo", maximo, "na posição ", position)'''
+
+'''numeros1=[-5,-2,5,7,8,9,1]
+minimo=min(numeros1)
+posicao=numeros1.index(minimo)
+print("menor valor é:", minimo, "na posição", posicao)'''
+
+'''numerosOrdenados=[-5,-2,5,7,8,9,1,3]
+ordenadas=sorted(numerosOrdenados)
+print(ordenada)'''
+
+'''somaNumeros=[5,6,9,4,5]
+soma=sum(somaNumeros)
+print('A soma da lista somaNumeros é:{}'.format(soma))'''
+
+'''letras=list('samuel')
+numero=int(input('Digite um numero: '))
+print(letras[:numero])'''
+'''
+palavra=input('Digite uma palavra: ')
+numero=int(input('Digite um numero: '))
+print(palavra[:numero+1])'''
+
+'''vetor=[3,6,9,12,15.8]
+inteiros=all(isinstance(y,int)for y in vetor)
+print(inteiros)'''
+
+'''vetor1=[0,13,17,26]
+media=sum(vetor1)/len(vetor1)
+acimaMedia=[w for w in vetor1 if w >= media]
+print(media)
+print('valores no vetor acima da media,', acimaMedia)'''
+
+'''vetorDobrado=[7,11,16]
+dobro=[m*2 for m in vetorDobrado]
+print(dobro)'''
+
+
+
+
+
+
