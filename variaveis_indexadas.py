add3b41a57305b05396d8b6026fd325d48baf06f
samueldmsource <samuelsjnmg@gmail.com>
2026-05-18 12:32:27 -0300

    second commit

--- variaveis_indexadas.py
+++ variaveis_indexadas.py
@@ -1,43 +1,128 @@
-'''numeros=[-5,-2,5,7,8,9]
-negativo=[i for i in numeros if i < 0]
-print(negativo)'''
 
-'''Dados=[4,7,8,9,25]
-maximo=max(Dados)
-position=Dados.index(maximo)
+'''while True:
+    numeros=list(map(int,input("digite valores: ").split()))#[-5,-2,5,7,8,9]
+    negativo=[i for i in numeros if i < 0]
+    print(negativo)
+    if 0 in numeros:
+        break'''
+from operator import index
 
-print("valor maximo", maximo, "na posição ", position)'''
+#print("pare")
+        #break
+'''dados=[]
+while True:
+    numeros=list(map(int,input("digite o valor: ").split()))#[4,7,8,9,25]
+    dados.append(numeros)
 
-'''numeros1=[-5,-2,5,7,8,9,1]
-minimo=min(numeros1)
-posicao=numeros1.index(minimo)
-print("menor valor é:", minimo, "na posição", posicao)'''
+    if dados:
+        maximo=max(dados)
+        position=dados.index(maximo)
+        if 0 in numeros:
+            break
+        print(f"valor maximo, {maximo}, na posição , {position}")'''
 
-'''numerosOrdenados=[-5,-2,5,7,8,9,1,3]
-ordenadas=sorted(numerosOrdenados)
-print(ordenada)'''
+'''dados=[]
+while True:
+    numeros=list(map(int,input("digite valores: ").split()))
+    dados.append(numeros)
+    if numeros==0:
+        break
+    if dados:
+        minimo=min(dados)
+        posicao=dados.index(minimo)
+        print("menor valor é:", minimo, "na posição", posicao)'''
 
-'''somaNumeros=[5,6,9,4,5]
-soma=sum(somaNumeros)
-print('A soma da lista somaNumeros é:{}'.format(soma))'''
+'''ordenacao=[]
+while True:
+    numerosOrdenados=list(map(int,input("digite valores: ").split()))
+    if numerosOrdenados==0:
+        break
+    ordenacao.append(numerosOrdenados)
+    if ordenacao:
+        ordenadas=sorted(ordenacao)
+        print(ordenadas)'''
 
-'''letras=list('samuel')
-numero=int(input('Digite um numero: '))
-print(letras[:numero])'''
-'''
-palavra=input('Digite uma palavra: ')
-numero=int(input('Digite um numero: '))
-print(palavra[:numero+1])'''
+'''somar=[]
+while True:
+    somaNumeros=int(input("digite um numero: "))
+    somar.append(somaNumeros)
+    soma = sum(somar)
+    print(soma)
+    if somaNumeros==0:
+        break'''
+#letras=[]
+'''while True:
+    #numeros=int(input("digite um numero: "))
+    palavra=input("digite uma palavra: ")
+    numero=int(input('Digite um numero: '))
+    #letras.append(palavra)
 
-'''vetor=[3,6,9,12,15.8]
-inteiros=all(isinstance(y,int)for y in vetor)
-print(inteiros)'''
+    if palavra:
+        print(palavra[:numero+1])
+    if palavra=='sair':
+        break'''
 
-'''vetor1=[0,13,17,26]
-media=sum(vetor1)/len(vetor1)
-acimaMedia=[w for w in vetor1 if w >= media]
-print(media)
-print('valores no vetor acima da media,', acimaMedia)'''
+'''while True:
+    letras=input("qual palavra? ")
+    numero=int(input('Digite um numero: '))
+    
+    if letras:
+        print(letras[:numero])'''
+
+#acumuladorVetor=[]
+'''while True:
+    try:
+        vetor=list(map(int,input("digite valores inteiros ou negativos: ").split()))
+        print(vetor, True)
+#inteiros = all(isinstance(y, int) for y in vetor)
+    except ValueError:
+        print(vetor, False)'''
+
+
+
+'''acimaMedia=[]
+while True:
+    vetor1=list(map(float,input("quais valores para cálculo da média? ").split()))
+    if vetor1:
+        media = sum(vetor1) / len(vetor1)
+        acimaMedia = [w for w in vetor1 if w > media]
+        print(media)
+        print('valores no vetor acima da media,', acimaMedia)
+        vetor1=list(map(float,input("quais valores para cálculo da média? ").split()))
+    else:
+        print("digite valores inteiros")
+        break'''
+
+'''acimaMedia=[]
+media=0
+while True:
+    vetor1 = list(map(float, input("Quais valores para cálculo da média? ").split()))
+
+    if vetor1:
+        media = sum(vetor1) / len(vetor1)
+        acimaMedia = [w for w in vetor1 if w > media]
+
+        print("Média =", media)
+        print("Valores no vetor acima da média:", acimaMedia)
+    else:
+        print("Digite valores inteiros ou decimais.")'''
+
+
+
+'''acimaMedia=[]
+valores=[]
+while True:
+    vetor1 = int(input("qual valor? "))
+    valores.append(vetor1)
+    
+    if valores:
+        media = sum(valores) / len(valores)
+        acimaMedia = [w for w in valores if w > media]
+        print(media)
+        print('valores no vetor acima da media,', acimaMedia)
+
+    else:
+        print("digite valores inteiros")'''
 
 '''vetorDobrado=[7,11,16]
 dobro=[m*2 for m in vetorDobrado]
